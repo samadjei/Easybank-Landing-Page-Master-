da18e9e5c93b2a8ae418074dcc2ee712eb35cfa3
Samuel Adjei <samueladj3i@gmail.com>
2020-05-30 00:24:00 +0100

    new commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Hamburger   Menu
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.menu');
+const overlay = document.querySelector('.bg-overlay')
 const links = document.querySelectorAll('.menu li');
 
 // Hamburger Menu
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('open');
+  overlay.classList.toggle('open');
   hamburger.classList.toggle('toggle');
 });
